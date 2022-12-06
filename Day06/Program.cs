49318e7b004a7718de7ce93641c15e7828f05dff
Rainer Stropek <rainer@timecockpit.com>
2022-12-06 06:10:30 +0100

    Add day 6

---
+++ Day06/Program.cs
@@ -0,0 +1,25 @@
+﻿//          +--- Add your data here
+//          |
+//          V
+var data = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+// === Part one ===
+Console.WriteLine(FindDistinct(data, 4));
+
+// === Part two ===
+Console.WriteLine(FindDistinct(data, 14));
+
+
+int FindDistinct(string data, int length)
+{
+    for (var i = length - 1; i < data.Length; i++)
+    {
+        var sub = data[(i - length + 1)..(i + 1)];
+        if (sub.Distinct().Count() == length)
+        {
+            return i + 1;
+        }
+    }
+
+    throw new InvalidOperationException();
+}
