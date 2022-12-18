96a5110cc19ff0db15499cfadfdc4ffca6b00f48
Rainer <rainer@software-architects.at>
2022-12-18 06:10:51 +0100

    D16

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -30,72 +30,151 @@ foreach (var v in valves.Values)
 {
     v.NextValves = v.NextValveIDs.Select(id => valves[id]).ToArray();
 }
+foreach (var v in valves.Values)
+{
+    v.Distances = FindAllDistances(v);
+}
+
+var opened = new HashSet<string>(valves.Where(v => v.Value.FlowRate == 0).Select(v => v.Key));
+var max = Open(valves["AA"], opened, 30, 0, 0);
 
 System.Console.WriteLine();
 
-// === Part one ===
-var maxPreassure = 0;
-Release(valves["AA"], new(), new(), 0, 0);
-
-// var analyze = preassureRelease
-//     .Where(p => p.path.Count >= 4 && p.path[0] == "DD" && p.path[1] == "BB" && p.path[1] == "JJ" && p.path[1] == "HH" && p.path[1] == "EE" && p.path[1] == "CC")
-//     .ToArray();
-
-Console.WriteLine(maxPreassure);
-
-void Release(Valve v, List<string> visited, List<string> opened, int elapsedMinutes, int releasedPreassure)
+int Open(Valve valve, HashSet<string> opened, int remainingTime, int current, int currentMax)
 {
-    if (elapsedMinutes >= 28)
+    if (remainingTime <= 1)
     {
-        if (maxPreassure < releasedPreassure)
-        {
-            System.Console.WriteLine(elapsedMinutes);
-            maxPreassure = releasedPreassure;
-        }
-        return;
+        return Math.Max(current, currentMax);
     }
 
-    var newVisited = new List<string>();
-    newVisited.AddRange(visited);
-    newVisited.Add(v.ID);
-    visited = newVisited;
-
-    if (v.FlowRate > 0)
+    if (valve.ID != "AA")
     {
-        var newOpened = new List<string>();
-        newOpened.AddRange(opened);
-        newOpened.Add(v.ID);
-        opened = newOpened;
+        opened.Add(valve.ID);
+        current += valve.FlowRate * (remainingTime - 1);
+        remainingTime--;
 
-        var newReleasedPreassure = releasedPreassure + v.FlowRate * (30 - 1 - elapsedMinutes);
+        var notOpened = valves!.Where(v => !opened.Contains(v.Key)).ToArray();
+        var theoreticalMax = notOpened.Any() ? (remainingTime - notOpened.Min(v => valve.Distances[v.Key]) - 1) * notOpened.Sum(v => valves![v.Key].FlowRate) : 0;
 
-        if ((newReleasedPreassure + valves.Where(v => !opened.Contains(v.Key)).Sum(v => v.Value.FlowRate * (30 - 2 - elapsedMinutes))) > maxPreassure)
+        if (current + theoreticalMax < currentMax)
         {
-            //preassureRelease.Add((opened, newReleasedPreassure));
-            foreach (var next in v.NextValves)
-            {
-                // if (!opened.Contains(next.ID))
-                {
-                    Release(next, visited, opened, elapsedMinutes + 2, newReleasedPreassure);
-                }
-            }
+            return currentMax;
         }
     }
 
-    //preassureRelease.Add((newPath, releasedPreassure));
-    if ((releasedPreassure + valves.Where(v => !opened.Contains(v.Key)).Sum(v => v.Value.FlowRate * (30 - 2 - elapsedMinutes))) > maxPreassure)
+    var max = 0;
+    var nextOpenings = valve.Distances
+        .Where(kv => !opened.Contains(kv.Key))
+        .Select(kv => (kv.Key, Flow: (remainingTime - 1 - kv.Value) * valves[kv.Key].FlowRate))
+        .OrderByDescending(kv => kv.Item2);
+    foreach(var next in nextOpenings)
     {
-        foreach (var next in v.NextValves)
+        var result = Open(valves![next.Key], new HashSet<string>(opened), remainingTime - valve.Distances[next.Key], current, Math.Max(current + max, currentMax));
+        if (result > max)
         {
-            // if (!opened.Contains(next.ID))
-            {
-                Release(next, visited, opened, elapsedMinutes + 1, releasedPreassure);
-            }
+            max = result;
+        }
+    }
+
+    return Math.Max(max, currentMax);
+}
+
+// === Part one ===
+
+
+
+// var maxPreassure = 0;
+// Release(valves["AA"], new(), new(), 0, 0);
+
+// // var analyze = preassureRelease
+// //     .Where(p => p.path.Count >= 4 && p.path[0] == "DD" && p.path[1] == "BB" && p.path[1] == "JJ" && p.path[1] == "HH" && p.path[1] == "EE" && p.path[1] == "CC")
+// //     .ToArray();
+
+// Console.WriteLine(maxPreassure);
+
+// void Release(Valve v, List<string> visited, List<string> opened, int elapsedMinutes, int releasedPreassure)
+// {
+//     if (elapsedMinutes >= 28)
+//     {
+//         if (maxPreassure < releasedPreassure)
+//         {
+//             System.Console.WriteLine(elapsedMinutes);
+//             maxPreassure = releasedPreassure;
+//         }
+//         return;
+//     }
+
+//     var newVisited = new List<string>();
+//     newVisited.AddRange(visited);
+//     newVisited.Add(v.ID);
+//     visited = newVisited;
+
+//     if (v.FlowRate > 0)
+//     {
+//         var newOpened = new List<string>();
+//         newOpened.AddRange(opened);
+//         newOpened.Add(v.ID);
+//         opened = newOpened;
+
+//         var newReleasedPreassure = releasedPreassure + v.FlowRate * (30 - 1 - elapsedMinutes);
+
+//         if ((newReleasedPreassure + valves.Where(v => !opened.Contains(v.Key)).Sum(v => v.Value.FlowRate * (30 - 2 - elapsedMinutes))) > maxPreassure)
+//         {
+//             //preassureRelease.Add((opened, newReleasedPreassure));
+//             foreach (var next in v.NextValves)
+//             {
+//                 // if (!opened.Contains(next.ID))
+//                 {
+//                     Release(next, visited, opened, elapsedMinutes + 2, newReleasedPreassure);
+//                 }
+//             }
+//         }
+//     }
+
+//     //preassureRelease.Add((newPath, releasedPreassure));
+//     if ((releasedPreassure + valves.Where(v => !opened.Contains(v.Key)).Sum(v => v.Value.FlowRate * (30 - 2 - elapsedMinutes))) > maxPreassure)
+//     {
+//         foreach (var next in v.NextValves)
+//         {
+//             // if (!opened.Contains(next.ID))
+//             {
+//                 Release(next, visited, opened, elapsedMinutes + 1, releasedPreassure);
+//             }
+//         }
+//     }
+// }
+
+IEnumerable<(string Key, int Flow)> FindMaxValve(Dictionary<string, Valve> valves, Valve from, int remainingMinutes, HashSet<string> opened)
+{
+    return from.Distances
+        .Where(kv => !opened.Contains(kv.Key))
+        .Select(kv => (kv.Key, Flow: (remainingMinutes - 1 - kv.Value) * valves[kv.Key].FlowRate))
+        .OrderByDescending(kv => kv.Item2);
+}
+
+Dictionary<string, int> FindAllDistances(Valve from)
+{
+    var distances = new Dictionary<string, int>
+    {
+        { from.ID, 0 }
+    };
+    FindAllDistancesImpl(from, distances, 1);
+    return distances;
+}
+
+void FindAllDistancesImpl(Valve from, Dictionary<string, int> distances, int distance)
+{
+    foreach (var next in from.NextValves)
+    {
+        if (!distances.ContainsKey(next.ID) || distances[next.ID] > distance)
+        {
+            distances[next.ID] = distance;
+            FindAllDistancesImpl(next, distances, distance + 1);
         }
     }
 }
 
-// === Part two ===
+// // === Part two ===
 
 
 static partial class Data
@@ -107,4 +186,5 @@ static partial class Data
 record Valve(string ID, int FlowRate, string[] NextValveIDs)
 {
     public Valve[] NextValves { get; set; }
+    public Dictionary<string, int> Distances { get; set; }
 }
